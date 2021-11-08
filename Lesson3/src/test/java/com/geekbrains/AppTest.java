5150cb141151271766951f6a3de1999719781162
Vyacheslav <iskandertrade@gmail.com>
2021-11-08 20:55:28 +0300

    DZ_03

---
+++ Lesson3/src/test/java/com/geekbrains/AppTest.java
@@ -0,0 +1,11 @@
+package com.geekbrains;
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue() { System.out.println("Я тест №1"); }
+
+    @Test
+    public void shouldAnswerWithTrue2() { System.out.println("Я тест №2"); }
+}
